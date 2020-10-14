09340b2c4bc1e97e5514ed13e44303bb9fcf6744
Vijay v <v.vijay@ASPIROMTECH.COM>
2020-10-14 21:40:30 +0530

    First commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Divo Scripts
+
+
+$(document).ready(function(){
+
+
+    //menu toggle
+
+    $('.menu_toggle').click(function(e){
+        e.preventDefault();
+        $(this).toggleClass('active');
+        $('.main_menu').toggleClass('active');
+    })
+})
