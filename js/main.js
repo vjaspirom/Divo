19189aa6893699689544fd330a1b09e00d9406bb
Vijay v <v.vijay@ASPIROMTECH.COM>
2020-10-18 22:57:15 +0530

    contact page updated

--- js/main.js
+++ js/main.js
@@ -7,6 +7,18 @@ $(document).ready(function(){
         duration: 700
     });
 
+    $('select').selectpicker();
+
+    // $('select').select2({
+    //     minimumResultsForSearch: -1
+    // });
+
+    // $('.form-col select').select2({
+    //     placeholder: "Language",
+    //     minimumResultsForSearch: -1
+
+    // });
+
     //menu toggle
 
     $('.menu_toggle').click(function(e){
@@ -14,5 +26,25 @@ $(document).ready(function(){
         $(this).toggleClass('active');
         $('.main_menu').toggleClass('active');
         $('body').toggleClass('noscroll');
+    })
+
+
+    // contact form switch
+
+    $('.choosebtn a').click(function(e){
+        e.preventDefault();
+        $('.choosebtn a').removeClass('active');
+
+        if($(this).hasClass('producer')){
+            $('.artist_form').removeClass('active');
+            $('.producer_form').addClass('active');
+            $(this).addClass('active');
+        }
+        else if($(this).hasClass('artist')){
+            $('.producer_form').removeClass('active');
+            $('.artist_form').addClass('active');
+            $(this).addClass('active');
+
+        }
     })
 })
